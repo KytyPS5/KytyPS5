27c20f4ebbab79418056e899333f76372a597fa2
nmzik <Nmzik@mail.ru>
2026-08-04 02:51:11 +0200

    fix(launcher): focus emulator window on launch

--- src/launcher/src/mainDialog.cpp
+++ src/launcher/src/mainDialog.cpp
@@ -42,9 +42,7 @@ constexpr char EMULATOR_EXE[] = "kyty_emulator.exe";
 constexpr char EMULATOR_EXE[] = "kyty_emulator";
 #endif
 
-#if defined(_WIN32)
-constexpr char CMD_EXE[] = "cmd.exe";
-#elif defined(__linux__)
+#if defined(__linux__)
 constexpr char KYTY_BASH_FILE[] = "kyty_run.sh";
 #endif
 #if defined(_WIN32)
@@ -359,14 +357,6 @@ void MainDialog::RunInterpreter(QProcess* process, const Configuration& info) {
 			process->setProgram(QStringLiteral("bash"));
 			process->setArguments({QStringLiteral("-c"), bash_file_name});
 		}
-	}
-#elif defined(_WIN32)
-	{
-		process->setProgram(CMD_EXE);
-		QStringList process_args;
-		process_args << QStringLiteral("/K") << interpreter;
-		process_args += args;
-		process->setArguments(process_args);
 	}
 #else
 	process->setProgram(interpreter);

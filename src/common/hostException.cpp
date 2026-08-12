0cd3b7f58bd79b471d309135d90ab7419d8f6a68
Pouare514 <julienaulycee@gmail.com>
2026-08-12 13:23:57 +0200

    fix: add handling for additional exception code in ExceptionFilter

--- src/common/hostException.cpp
+++ src/common/hostException.cpp
@@ -79,7 +79,8 @@ static LONG WINAPI ExceptionFilter(PEXCEPTION_POINTERS exception) {
 	auto* exception_record = exception->ExceptionRecord;
 
 	if (exception_record->ExceptionCode == DBG_PRINTEXCEPTION_C ||
-	    exception_record->ExceptionCode == DBG_PRINTEXCEPTION_WIDE_C) {
+	    exception_record->ExceptionCode == DBG_PRINTEXCEPTION_WIDE_C ||
+	    exception_record->ExceptionCode == 0xE06D7363) {
 		return EXCEPTION_CONTINUE_SEARCH;
 	}
 

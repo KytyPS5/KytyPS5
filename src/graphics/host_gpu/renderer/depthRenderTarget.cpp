79af497d126e3ffb3fa9cb8c984cab0fc80faf73
TarkusR <90155059+TarkusR@users.noreply.github.com>
2026-09-07 00:12:28 +0200

    renderer: clear depth and stencil from the HTILE fill value

--- src/graphics/host_gpu/renderer/depthRenderTarget.cpp
+++ src/graphics/host_gpu/renderer/depthRenderTarget.cpp
@@ -250,7 +250,8 @@ void RenderExecutor::ResolveRenderDepthTarget(uint64_t submit_id, CommandBuffer&
 
 	r.stencil_clear_enable =
 	    has_stencil && rc.stencil_clear_enable && !z.depth_view.stencil_write_disable;
-	r.stencil_clear_value = hw.GetStencilClearValue();
+	r.stencil_clear_value       = hw.GetStencilClearValue();
+	r.stencil_meta_clear_enable = false;
 	r.stencil_test_enable = has_stencil && dc.stencil_enable;
 	if (r.stencil_test_enable) {
 		const bool stencil_ops_disabled =
@@ -369,7 +370,7 @@ vk::ImageAspectFlags RenderDepthInfo::AttachmentWriteAspects() const {
 		       (can_pass && depth_pass && state.passOp != vk::StencilOp::eKeep) ||
 		       (can_pass && depth_fail && state.depthFailOp != vk::StencilOp::eKeep);
 	};
-	if (stencil_clear_enable ||
+	if (stencil_clear_enable || stencil_meta_clear_enable ||
 	    (stencil_test_enable && (face_writes(stencil_static_front, stencil_dynamic_front) ||
 	                             face_writes(stencil_static_back, stencil_dynamic_back)))) {
 		writes |= vk::ImageAspectFlagBits::eStencil;

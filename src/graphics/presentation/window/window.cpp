e12fe9e2c0c1d3f212ac2ab396efb385ac4c9673
nmzik <Nmzik@mail.ru>
2026-09-23 17:47:30 +0400

    Allow window resizing by default and recreate swapchain on resize

--- src/graphics/presentation/window/window.cpp
+++ src/graphics/presentation/window/window.cpp
@@ -783,14 +783,13 @@ static void WindowCreate(WindowContext& context) {
 	}
 
 	SDL_SetWindowPosition(context.window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-	SDL_SetWindowResizable(context.window, false);
 	HostInputInit(context.window);
 	InitializeSystemOverlayInput(context.window);
 	context.UpdateIcon();
 }
 
 uint32_t WindowContext::InitialWindowFlags(bool fullscreen) noexcept {
-	auto flags = static_cast<uint32_t>(SDL_WINDOW_VULKAN);
+	auto flags = static_cast<uint32_t>(SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
 	if (fullscreen) {
 		flags |= static_cast<uint32_t>(SDL_WINDOW_FULLSCREEN);
 	}
